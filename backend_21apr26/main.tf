208871b3b584c9cdc69ef9dbb11831e2b055435a
Hkamboj12 <himanshudattana09@gamil.com>
2026-04-21 23:12:41 +0530

    store tfstate file to azure storage account in blob container

---
+++ backend_21apr26/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg1" {
+    name = "rg-dev"
+    location = "Central India"
+
+    tags = {
+        env = "dev"
+    }
+}
+
+
+resource "azurerm_storage_account" "sg-stateFile" {
+  name                     = "statefilehimanshu"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    type = "stateFile"
+  }
+}
+resource "azurerm_storage_container" "blob-statefile" {
+  name                  = "stateFile-container"
+  storage_account_id    = azurerm_resource_group.rg1.id
+  container_access_type = "private"
+}
