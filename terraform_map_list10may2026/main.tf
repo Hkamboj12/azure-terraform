afb71a783a8f7f921af43d7f1797173e144a7a55
Hkamboj12 <himanshudattana09@gamil.com>
2026-05-12 21:39:03 +0530

    modify the value in map

--- terraform_map_list10may2026/main.tf
+++ terraform_map_list10may2026/main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "rg" {
   location = each.value
 }
 
-# resource "azurerm_storage_account" "sg" {
-#     for_each = var.storage_group
-#     name = each.key.name
-#     resource_group_name = each.key.resource_group_name 
-#     location = each.key.loction
-#     account_tier = each.key.account_tier
-#     account_replication_type = each.key.account_replication_type
-# }
+resource "azurerm_storage_account" "sg" {
+    for_each = var.storage_group
+    name = each.value.name
+    resource_group_name = each.value.resource_group_name 
+    location = each.value.location
+    account_tier = each.value.account_tier
+    account_replication_type = each.value.account_replication_type
+}
 
 # resource "azurerm_virtual_machine" "VM" {
 #   name                  = ""
--- terraform_map_list10may2026/variables.tf
+++ terraform_map_list10may2026/variables.tf
@@ -6,15 +6,15 @@ variable "rg" {
     }  
 }
 
-# variable "storage_group" {
-#     type = map(string)
-#     default = {
-#         stg = {
-#             name = "himanshustroage123456"
-#             location = "centralindia"
-#             resource_group_name = "dev"
-#             account_tier = "Standard"
-#             account_replication_type = "GRS"
-#         }
-#     }  
-# }
+variable "storage_group" {
+    type = map
+    default = {
+        stg = {
+            name = "himanshustroage123456"
+            location = "centralindia"
+            resource_group_name = "dev"
+            account_tier = "Standard"
+            account_replication_type = "GRS"
+        }
+    }  
+}
