afb8189a325e233b0b8ee388e45f05f65764116b
Hkamboj12 <himanshudattana09@gamil.com>
2026-05-20 22:59:18 +0530

    learing how to use of backup file and tfstate file

---
+++ terraform_starting_to_end/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "rg-1" {
+  name     = "rg-1hims"
+  location = "centralindia"
+}
+
+
+resource "azurerm_resource_group" "rg-2" {
+  name     = "rg-1himan"
+  location = "centralindia"
+}
---
+++ terraform_statefile/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "rg-1" {
+  name     = "rg-1hims"
+  location = "centralindia"
+}
+
+resource "azurerm_resource_group" "rg-2" {
+  name     = "rg-1himan"
+  location = "centralindia"
+}
+
+resource "azurerm_resource_group" "rg-3" {
+  name     = "rg-1himanka"
+  location = "centralindia"
+}
